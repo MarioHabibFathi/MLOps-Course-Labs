d03df6e1c04ead63572999dc9fe8c7634f784db3
MarioHabibFathi <mariohabibfathy@gmail.com>
2025-05-21 03:49:13 +0300

    Added docker to the project

--- src/model.py
+++ src/model.py
@@ -1,9 +1,10 @@
+import os
 import mlflow
 import mlflow.sklearn
 
 MODEL_NAME = "SVM prediction"
 
-mlflow.set_tracking_uri("http://localhost:5000")
+mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI", "http://localhost:5000"))
 
 def load_model():
     model = mlflow.sklearn.load_model(f"models:/{MODEL_NAME}/Production")
