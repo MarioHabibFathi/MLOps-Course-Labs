083cce47d4b6eaf1692d347f14a29a92b110285c
MarioHabibFathi <mariohabibfathy@gmail.com>
2025-05-22 22:11:22 +0300

    Added Prometheus and grafana for monitoring

--- src/model.py
+++ src/model.py
@@ -6,7 +6,7 @@ import tempfile
 
 MODEL_NAME = "SVM prediction"
 
-mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI", "http://localhost:5000"))
+mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI", "http://mlflow:5000"))
 
 def load_model():
     model = mlflow.sklearn.load_model(f"models:/{MODEL_NAME}/Production")
